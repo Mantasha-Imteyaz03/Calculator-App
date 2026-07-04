af4ec8870332c792c165ee1cab9f224b87a0e8f1
Mantasha Imteyaz <mantashaimteyaz30@gmail.com>
2026-07-04 21:05:02 +0530

    complete calculator

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let btns = document.querySelector('.btns')
-let btnArray = ['C','DE','%','/','7','8','9','*','4','5','6','-','1','2','3','+','0','.','='];
+const btnArray = ['C','DE','%','/','7','8','9','*','4','5','6','-','1','2','3','+','0','.','='];
 
-btnArray.map((btn)=>{
+let input = document.querySelector('.calculator input')
+
+btnArray.forEach((btn)=>{
  const button = document.createElement('button')
- button.innerHTML = btn
+ button.textContent = btn
   if(btn === '0'){
   button.classList.add('zero')
  }
  btns.appendChild(button)
 
+ button.addEventListener('click',()=>{
+  input.focus()
+  if(btn === 'C' || btn === 'c'){
+  input.value = ''
+ }else if(btn==='DE'){
+  let len = input.value.length;
+  let newValue = input.value.slice(0,len-1)
+  input.value = newValue
+  }else if(btn === '='){
+   try {
+    input.value =  eval(input.value)
+   } 
+   catch (error) {
+    console.log(error);
+   }
+  }
+ else{
+input.value += btn
+ }
+ })
 })
+
